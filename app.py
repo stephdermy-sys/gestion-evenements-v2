eee9d9fa055fe98d00bfa273d07562749ae6a597
stephdermy-sys <stephdermy@gmail.com>
2026-03-30 18:25:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+
+    if request.method == "POST":
+
+        return """
+        <h2>Votre demande a bien été envoyée.</h2>
+
+        <p>
+        Le service Communication vous contactera prochainement.
+        </p>
+
+        <br>
+
+        <a href="/">Faire une nouvelle demande</a>
+        """
+
     return render_template("formulaire.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
